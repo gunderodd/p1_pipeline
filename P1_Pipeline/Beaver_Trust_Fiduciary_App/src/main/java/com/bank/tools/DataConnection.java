389e6d2bf3760d4c7b637ce9f6330d5c2a04e66e
gunderodd <gunderodd@hotmail.com>
2020-05-28 14:49:19 -0700

    new test

--- P1_Pipeline/Beaver_Trust_Fiduciary_App/src/main/java/com/bank/tools/DataConnection.java
+++ P1_Pipeline/Beaver_Trust_Fiduciary_App/src/main/java/com/bank/tools/DataConnection.java
@@ -23,7 +23,7 @@ public class DataConnection {
 //	private static final String USERNAME = System.getenv("REVATURE_P0_USERNAME");
 //	private static final String PASSWORD = System.getenv("REVATURE_P0_PASSWORD");
 	
-	public static Connection getConnection() throws SQLException {
+	public Connection getConnection() throws SQLException {
 		//original approach, will delete to simulate security if system variables work
 		String URL = 
 				"jdbc:oracle:thin:@database-1.cb5vpvii5jvy.us-west-2.rds.amazonaws.com:1521:orcl";
