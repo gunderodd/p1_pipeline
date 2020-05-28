e2cf0444dc4768aba13fbd487fff4664ed31b0fa
gunderodd <gunderodd@hotmail.com>
2020-05-28 08:39:19 -0700

    hardcoded connection values

--- P1_Pipeline/Beaver_Trust_Fiduciary_App/src/main/java/com/bank/tools/DataConnection.java
+++ P1_Pipeline/Beaver_Trust_Fiduciary_App/src/main/java/com/bank/tools/DataConnection.java
@@ -19,16 +19,16 @@ public class DataConnection {
 	}
 	
 	//using my system variables, hiding my info from github
-	private static final String URL = System.getenv("REVATURE_P0_URL");
-	private static final String USERNAME = System.getenv("REVATURE_P0_USERNAME");
-	private static final String PASSWORD = System.getenv("REVATURE_P0_PASSWORD");
+//	private static final String URL = System.getenv("REVATURE_P0_URL");
+//	private static final String USERNAME = System.getenv("REVATURE_P0_USERNAME");
+//	private static final String PASSWORD = System.getenv("REVATURE_P0_PASSWORD");
 	
 	public static Connection getConnection() throws SQLException {
 		//original approach, will delete to simulate security if system variables work
-//		String url = 
-//				"jdbc:oracle:thin:@database-1.cb5vpvii5jvy.us-west-2.rds.amazonaws.com:1521:orcl";
-//		String username = "admin";
-//		String password = "gunderodd";
+		String URL = 
+				"jdbc:oracle:thin:@database-1.cb5vpvii5jvy.us-west-2.rds.amazonaws.com:1521:orcl";
+		String USERNAME = "admin";
+		String PASSWORD = "gunderodd";
 		
 		// this fix allows me to use my connection in P1
 		try {
